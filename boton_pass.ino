111c83c7b5004b5c7c98b526e23fab54545d54e6
Alejandro Rabadán Camacho <arabadan70@hotmail.com>
2016-06-19 06:07:35 -0500

    El programa tiene contadores los cuales si matchean con la cantidad que tiene la password hace que prenda el led

--- boton_pass.ino
+++ boton_pass.ino
@@ -9,29 +9,81 @@ int estadoBoton1 = 0;  // estado del botón (0 ó LOW es apagado y 1 ó HIGH es 
 int estadoBoton2 = 0;
 int estadoBotonSubmit = 0;
 
+int contadorBoton1 = 0;
+int contadorBoton2 = 0;
+
+int primerPassword()
+{
+	int passBoton1 = 1;
+	int passBoton2 = 1;
+
+	if(contadorBoton1 == passBoton1 && contadorBoton2 == passBoton2)
+		return 1;
+	else
+		return 0;
+}
+
 void setup() {
-  // Se identifica el pin 13 como salida
-  pinMode(led1, OUTPUT);     
-  pinMode(led2, OUTPUT);    
-  // Se identifica el pin 2 como entrada
-  pinMode(boton1, INPUT);  
-  pinMode(boton2, INPUT);  
-  pinMode(botonSubmit, INPUT);     
+	Serial.begin(9600);
+
+	// Se identifica el pin 13 como salida
+ 	pinMode(led1, OUTPUT);     
+ 	pinMode(led2, OUTPUT);    
+ 	// Se identifica el pin 2 como entrada
+ 	pinMode(boton1, INPUT);  
+ 	pinMode(boton2, INPUT);  
+ 	pinMode(botonSubmit, INPUT);     
 }
 
 void loop(){
-  // Leemos si el botón en pin2 está abierto o cerrado
-  estadoBoton1 = digitalRead(boton1);
-  estadoBoton2 = digitalRead(boton2);
-  estadoBotonSubmit = digitalRead(botonSubmit);
+	delay(1000);
+  Serial.println("Inicio");
+	Serial.print("Presiones del boton1: ");
+	Serial.println(contadorBoton1);
+	Serial.print("Presiones del boton2: ");
+	Serial.println(contadorBoton2);
 
-  // Si está siendo pulsado es HIGH
-  if (estadoBoton1 == HIGH) {     
-      // Y el LED se enciende    
-      digitalWrite(led1, HIGH);  
-  } 
-  else {
-      // Si no es asi, se apaga
-      digitalWrite(led1, LOW); 
-  }
+	// Leemos si el botón en pin2 está abierto o cerrado
+	estadoBoton1 = digitalRead(boton1);
+	estadoBoton2 = digitalRead(boton2);
+	estadoBotonSubmit = digitalRead(botonSubmit);
+
+	if(estadoBotonSubmit == LOW)
+	{
+		if(estadoBoton1 == HIGH)
+		{
+			contadorBoton1++;
+		}
+		if(estadoBoton2 == HIGH)
+		{
+			contadorBoton2++;
+		}
+	}
+	else
+	{
+		if(primerPassword() == 1)
+		{
+			digitalWrite(led1, HIGH); 
+		}
+		else {
+	   		// Si no es asi, se apaga
+	   		digitalWrite(led1, LOW); 
+ 		}
+	}
+	delay(1000);
+	Serial.println("Final");
+	Serial.print("Presiones del boton1: ");
+	Serial.println(contadorBoton1);
+	Serial.print("Presiones del boton2: ");
+	Serial.println(contadorBoton2);
+	
+/*
+ 	if (estadoBoton1 == HIGH) {     
+   		digitalWrite(led1, HIGH);  
+ 	} 
+ 	else {
+   		digitalWrite(led1, LOW); 
+ 	}
+ */
 }
+
